081822f71c382303259f07253f0e1ca14e38fb51
Armin <arminmesic@hotmail.de>
2014-08-23 15:35:05 +0200

    Just show final solution

--- Problem1.js
+++ Problem1.js
@@ -4,6 +4,8 @@ do {
   i += 1
   if ( i % 3 === 0 || i % 5 === 0 || i % 15 === 0) {
     maxNumber += i
-    console.log( maxNumber)
+    if ( i == 999 ) {
+      console.log(maxNumber)
+    };
   };
 } while ( i < 999);
